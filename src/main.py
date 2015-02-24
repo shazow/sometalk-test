46c3a041f64e14b160fe90a304f49309ed0c5e73
Andrey Petrov <shazow@gmail.com>
2015-02-24 14:25:26 -0800

    Importing code.

---
+++ src/main.py
@@ -0,0 +1,10 @@
+"""
+Hey there.
+
+If you found this, send a pull request to this repo removing
+this file and I'll send you $5.
+
+If you're too lazy to collect such little money, then just
+open an issue and you can have bragging rights of finding
+it before other people did.
+"""
